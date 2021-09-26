e2a49dc0c9a4a9b1772677cc757b38a4ac6706a8
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-09-25 21:23:33 -0300

    Add a catalog service

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,48 +1,29 @@
 import Route from '@ember/routing/route';
-import { tracked } from '@glimmer/tracking';
-
-export class Band {
-  @tracked name;
-  @tracked songs;
-
-  constructor({ id, name, songs }) {
-    this.id = id;
-    this.name = name;
-    this.songs = songs;
-  }
-}
-
-export class Song {
-  constructor({ title, rating, band }) {
-    this.title = title;
-    this.rating = rating ?? 0;
-    this.band = band;
-  }
-}
+import { inject as service } from '@ember/service';
+import Band from 'rock-and-roll/models/band';
+import Song from 'rock-and-roll/models/song';
 
 export default class BandsRoute extends Route {
-  model() {
+  @service catalog;
+
+  #fillCatalog() {
     let blackDog = new Song({
       title: 'Black Dog',
-      band: 'Led Zeppelin',
       rating: 3,
     });
 
     let yellowLedbetter = new Song({
       title: 'Yellow Ledbetter',
-      band: 'Pearl Jam',
       rating: 4,
     });
 
     let pretender = new Song({
       title: 'The Pretender',
-      band: 'Foo Fighters',
       rating: 2,
     });
 
     let daughter = new Song({
       title: 'Daughter',
-      band: 'Pearl Jam',
       rating: 5,
     });
 
@@ -64,6 +45,24 @@ export default class BandsRoute extends Route {
       songs: [pretender],
     });
 
-    return [ledZeppelin, pearlJam, fooFighters];
+    blackDog.band = ledZeppelin;
+    yellowLedbetter.band = pearlJam;
+    daughter.band = pearlJam;
+    pretender.band = fooFighters;
+
+    this.catalog.add('song', blackDog);
+    this.catalog.add('song', yellowLedbetter);
+    this.catalog.add('song', daughter);
+    this.catalog.add('song', pretender);
+
+    this.catalog.add('band', ledZeppelin);
+    this.catalog.add('band', fooFighters);
+    this.catalog.add('band', pearlJam);
+  }
+
+  model() {
+    this.#fillCatalog();
+
+    return this.catalog.bands;
   }
 }
