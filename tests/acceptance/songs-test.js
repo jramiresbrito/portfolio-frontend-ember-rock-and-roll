504b83ba2d08fac72c10d8db562d49b739d70736
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-10-02 20:05:41 -0300

    TDD-RED: Write tests to use the sorting feature via UI

--- tests/acceptance/songs-test.js
+++ tests/acceptance/songs-test.js
@@ -62,5 +62,38 @@ module('Acceptance | Songs', function (hooks) {
         'Spinning in Daffodils',
         'The last song is the one that comes last in the alphabet'
       );
+
+    await click('[data-test-rr=sort-by-title-desc]');
+    assert
+      .dom('[data-test-rr=song-list-item]:first-child')
+      .hasText(
+        'Spinning in Daffodils',
+        'The first song is the one that comes last in the alphabet'
+      );
+    assert
+      .dom('[data-test-rr=song-list-item]:last-child')
+      .hasText(
+        'Elephants',
+        'The last song is the one that comes first in the alphabet'
+      );
+
+    await click('[data-test-rr=sort-by-rating-asc]');
+    assert
+      .dom('[data-test-rr=song-list-item]:first-child')
+      .hasText('Mind Eraser, No Chaser', 'The first song is the lowest rated');
+    assert
+      .dom('[data-test-rr=song-list-item]:last-child')
+      .hasText('Spinning in Daffodils', 'The last song is the highest rated');
+
+    await click('[data-test-rr=sort-by-rating-desc]');
+    assert
+      .dom('[data-test-rr=song-list-item]:first-child')
+      .hasText('Spinning in Daffodils', 'The first song is the highest rated');
+    assert
+      .dom('[data-test-rr=song-list-item]:last-child')
+      .hasAnyText(
+        'Mind Eraser, No Chaser',
+        'The last song is the lowest rated'
+      );
   });
 });
