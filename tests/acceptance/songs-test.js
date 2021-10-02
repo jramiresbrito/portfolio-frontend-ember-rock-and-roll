5ffdeb2f0e22cc15f856327f6f3babc232e70e64
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-10-02 19:52:38 -0300

    TDD: Add test related to Sorting Songs feature

--- tests/acceptance/songs-test.js
+++ tests/acceptance/songs-test.js
@@ -22,4 +22,45 @@ module('Acceptance | Songs', function (hooks) {
       .dom('[data-test-rr=song-list-item]')
       .exists({ count: 1 }, 'The new song is created');
   });
+
+  test('Sort songs in various ways', async function (assert) {
+    let band = this.server.create('band', { name: 'Them Crooked Vultures' });
+
+    this.server.create('song', {
+      title: 'Mind Eraser, No Chaser',
+      rating: 2,
+      band,
+    });
+    this.server.create('song', {
+      title: 'Elephants',
+      rating: 4,
+      band,
+    });
+    this.server.create('song', {
+      title: 'Spinning in Daffodils',
+      rating: 5,
+      band,
+    });
+    this.server.create('song', {
+      title: 'New Fang',
+      rating: 3,
+      band,
+    });
+
+    await visit('/');
+    await click('[data-test-rr=band-link]');
+
+    assert
+      .dom('[data-test-rr=song-list-item]:first-child')
+      .hasText(
+        'Elephants',
+        'The first song is the one that comes first in the alphabet'
+      );
+    assert
+      .dom('[data-test-rr=song-list-item]:last-child')
+      .hasText(
+        'Spinning in Daffodils',
+        'The last song is the one that comes last in the alphabet'
+      );
+  });
 });
